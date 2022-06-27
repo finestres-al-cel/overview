cae13615af694f8a5c211c38924e7ef3c778fcc4
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2022-06-27 20:03:54 +0200

    Added inatall script

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # overview
 Instructions to create an environment to run the different codes
 
-# Installation 
+# Installation
 1. Download miniconda from https://docs.conda.io/en/latest/miniconda.html
 2. Install with `bash Miniconda3-latest-Linux-x86_64.sh`
 3. run `./install.sh`
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install make
+sudo apt install gfortran
+conda create --name py3 python=3.9
+conda activate py
+conda install pip
+pip install jupyterlab
+pip install matplotlib astropy scipy numpy pandas
+cd ..
+git clone https://github.com/finestres-al-cel/pyspec.git
+git clone https://github.com/finestres-al-cel/titius_bode.git
+git clone https://github.com/finestres-al-cel/std_candle.git
+git clone https://github.com/finestres-al-cel/Practicasims.git
+git clone https://github.com/finestres-al-cel/cumulobert.git
