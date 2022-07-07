41ce54e70c205119f49d5930fffb72ee40c93bae
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2022-07-07 10:43:19 +0200

    added rawpy to install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ conda create --name py3 python=3.9
 conda activate py3
 conda install pip
 pip install jupyterlab
-pip install matplotlib astropy scipy numpy pandas
+pip install matplotlib astropy scipy numpy pandas rawpy
 cd ..
 git clone https://github.com/finestres-al-cel/pyspec.git
 git clone https://github.com/finestres-al-cel/titius_bode.git
