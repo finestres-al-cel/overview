d93fa0d1e63b559da248895a773473156a7a6a11
Santiastro1 <55127793+Santiastro1@users.noreply.github.com>
2023-07-01 18:31:05 +0200

    Update install.sh
    Update

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 sudo apt install make
 sudo apt install gfortran
 sudo apt install ffmpeg
+sudo apt install vlc
 conda create --name py3 python=3.9
 conda activate py3
 conda install pip
 pip install jupyterlab
+pip install notebook
 pip install matplotlib astropy scipy numpy pandas rawpy
 cd ..
 git clone https://github.com/finestres-al-cel/pyspec.git
