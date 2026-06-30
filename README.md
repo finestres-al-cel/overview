ae369460431be5d39b1e0ef746eda6de63e39882
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2026-06-30 22:19:43 +0200

    updated instructions

--- README.md
+++ README.md
@@ -4,16 +4,9 @@ Instructions to create an environment to run the different codes
 # Installation
 1. Download miniconda from https://docs.conda.io/en/latest/miniconda.html
 2. Install with `bash Miniconda3-latest-Linux-x86_64.sh`
+3. Create a conda environment. You may choose your preferred python VERSION. The codes are known to work with version 3.13, but this may change in the future. Run: `conda create --name my_env python=VERSION`
+4. Activate the environment: `conda activate my_env`
 3. run `sudo bash install_with_sudo.sh`
 4. run `bash install.sh`
-5. add the following lines to your bash profile (~/.profile or ~/.bash_profile):
-   
-     `EXPORT PYSPEC={INSTALL_PATH}/pyspec`
-   
-     `EXPORT CUMULOBERT={INSTALL_PATH}/cumulobert`
-   
-     `EXPORT PYTHONPATH=$PYTHONPATH':'$PYSPEC'/:'$CUMULOBERT'/:'`
-6. relaunch your terminal
-
 
 (replace {INSTALL_PATH} with the actual path where you cloned this repository)
--- install.sh
+++ install.sh
@@ -5,13 +5,30 @@
 #sudo apt install gfortran
 #sudo apt install ffmpeg
 #sudo apt install vlc
-conda create --name py3 python=3.9
-conda activate py3
-conda install pip
-pip install jupyterlab notebook matplotlib astropy scipy numpy pandas rawpy PyQt6 pyqtgraph photutils astroquery
 cd ..
-git clone https://github.com/finestres-al-cel/pyspec.git
-git clone https://github.com/finestres-al-cel/titius_bode.git
-git clone https://github.com/finestres-al-cel/std_candle.git
-git clone https://github.com/finestres-al-cel/Practicasims.git
-git clone https://github.com/finestres-al-cel/cumulobert.git
+#git clone https://github.com/finestres-al-cel/cumulobert.git
+cd cumulobert
+pip install -r requirements.txt
+pip install -e .
+cd ..
+#git clone https://github.com/finestres-al-cel/pyspec.git
+cd pyspec
+pip install -r requirements.txt
+pip install -e .
+cd ..
+#git clone https://github.com/finestres-al-cel/planet_orbits.git
+cd planet_orbits
+pip install -r requirements.txt
+pip install -e .
+cd ..
+#git clone https://github.com/finestres-al-cel/titius_bode.git
+cd titius_bode
+pip install -r requirements.txt
+cd ..
+#git clone https://github.com/finestres-al-cel/std_candle.git
+cd std_candle
+pip install -r requirements.txt
+cd ..
+#git clone https://github.com/finestres-al-cel/Practicasims.git
+cd Practicasims
+pip install -r requirements.txt
