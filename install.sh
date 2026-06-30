129eabea9b5a90d135ce32cd220e04c699720be8
Ignasi Pérez-Ràfols <iprafols@gmail.com>
2026-06-30 22:24:13 +0200

    removed debugging comments

--- install.sh
+++ install.sh
@@ -6,29 +6,29 @@
 #sudo apt install ffmpeg
 #sudo apt install vlc
 cd ..
-#git clone https://github.com/finestres-al-cel/cumulobert.git
+git clone https://github.com/finestres-al-cel/cumulobert.git
 cd cumulobert
 pip install -r requirements.txt
 pip install -e .
 cd ..
-#git clone https://github.com/finestres-al-cel/pyspec.git
+git clone https://github.com/finestres-al-cel/pyspec.git
 cd pyspec
 pip install -r requirements.txt
 pip install -e .
 cd ..
-#git clone https://github.com/finestres-al-cel/planet_orbits.git
+git clone https://github.com/finestres-al-cel/planet_orbits.git
 cd planet_orbits
 pip install -r requirements.txt
 pip install -e .
 cd ..
-#git clone https://github.com/finestres-al-cel/titius_bode.git
+git clone https://github.com/finestres-al-cel/titius_bode.git
 cd titius_bode
 pip install -r requirements.txt
 cd ..
-#git clone https://github.com/finestres-al-cel/std_candle.git
+git clone https://github.com/finestres-al-cel/std_candle.git
 cd std_candle
 pip install -r requirements.txt
 cd ..
-#git clone https://github.com/finestres-al-cel/Practicasims.git
+git clone https://github.com/finestres-al-cel/Practicasims.git
 cd Practicasims
 pip install -r requirements.txt
